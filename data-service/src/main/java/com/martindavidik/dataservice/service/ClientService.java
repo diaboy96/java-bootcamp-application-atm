3526787645d629c40f725b4363a77f1e49370d7d
Martin Davidík <martin.davidik@lundegaard.eu>
2023-09-19 17:22:50 +0200

    Create service layer for Account, Card, Client objects

---
+++ data-service/src/main/java/com/martindavidik/dataservice/service/ClientService.java
@@ -0,0 +1,4 @@
+package com.martindavidik.dataservice.service;
+
+public interface ClientService {
+}
---
+++ data-service/src/main/java/com/martindavidik/dataservice/service/impl/CardImpl.java
@@ -0,0 +1,8 @@
+package com.martindavidik.dataservice.service.impl;
+
+import com.martindavidik.dataservice.service.CardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardImpl implements CardService {
+}
